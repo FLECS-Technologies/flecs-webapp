0bf6cb6068075892c6eddc482125d5b03e24152b
Sam <89913680+samuel-greising@users.noreply.github.com>
2021-11-11 13:53:27 +0100

    Added some more apps...

--- src/data/DeviceAppsData.js
+++ src/data/DeviceAppsData.js
@@ -15,11 +15,27 @@ export let deviceAppsData = [
     ]
   },
   {
-    appId: "com.eclipse.mosquitto",
+    appId: "com.codesys.codesysgateway",
     status: "installed",
+    app: "CODESYS Edge Gateway",
+    manufacturer: "CODESYS GmbH",
+    version: "4.1.0",
+    instances: []
+  },
+  {
+    appId: "com.eclipse.mosquitto",
+    status: "uninstalled",
     app: "Mosquitto MQTT",
     manufacturer: "Eclipse Foundation",
     version: "2.0.13",
+    instances: []
+  },
+  {
+    appId: "com.mirasoft.anvizcloudadapter",
+    status: "installed",
+    app: "AnyViz Cloud Adapter",
+    manufacturer: "Mirasoft GmbH & Co. KG",
+    version: "6.3.9",
     instances: []
   }
 ];
--- src/data/MarketplaceData.js
+++ src/data/MarketplaceData.js
@@ -7,7 +7,7 @@ export let appData = [
     vendor: "CODESYS GmbH",
     version: "4.2.0",
     description: "IEC61131-3 Runtime.",
-    status: "uninstalled"
+    status: "installed"
   },
   {
     appId: "com.codesys.codesysgateway",
@@ -17,7 +17,7 @@ export let appData = [
     vendor: "CODESYS GmbH",
     version: "4.1.0",
     description: "Gateway to connect to CODESYS RTS.",
-    status: "uninstalled"
+    status: "installed"
   },
   {
     appId: "com.eclipse.mosquitto",
@@ -27,7 +27,7 @@ export let appData = [
     vendor: "Eclipse Foundation",
     version: "2.0.13",
     description: "MQTT broker.",
-    status: "uninstalled"
+    status: "installed"
   },
   {
     appId: "com.frankaemika.world",
@@ -57,6 +57,55 @@ export let appData = [
     vendor: "Mirasoft GmbH & Co. KG",
     version: "6.3.9",
     description: "Adapter to the AnyViz Cloud.",
+    status: "installed"
+  },
+  {
+    appId: "com.openjsfoundation.nodered",
+    avatar: "https://nodered.org/about/resources/media/node-red-icon.png",
+    title: "Node-Red",
+    vendor: "OpenJS Foundation",
+    version: "2.1.3",
+    description: "Low code programming.",
+    status: "uninstalled"
+  },
+  {
+    appId: "com.honeywell.matriconopcua",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1133980019/squiggle_icon_400x400.png",
+    title: "Matricon OPC UA",
+    vendor: "Honeywell International Inc.",
+    version: "2.1.3",
+    description: "OPC UA Stack.",
+    status: "uninstalled"
+  },
+  {
+    appId: "com.influxdata.influxdb",
+    avatar:
+      "https://influxdata.github.io/branding/img/downloads/influxdata-logo--symbol--pool.svg",
+    title: "InfluxDB",
+    vendor: "InfluxData Inc.",
+    version: "2.1.0",
+    description: "Time series data platform.",
+    status: "uninstalled"
+  },
+  {
+    appId: "com.teamviewer.teamvieweriot",
+    avatar:
+      "https://static.teamviewer.com/resources/2019/07/TeamViewer_Logo_512x512.png",
+    title: "TeamViewer IoT",
+    vendor: "TeamViewer Germany GmbH",
+    version: "5.8.0",
+    description: "Easy fleet management.",
+    status: "uninstalled"
+  },
+  {
+    appId: "com.inductiveautomation.ignition",
+    avatar:
+      "https://store-images.s-microsoft.com/image/apps.42258.faa5b89b-233a-4809-85c3-0859706a9831.65aa2a1c-12c2-40d4-b65f-a3ac1cf5ef3f.c95e2301-35cb-4723-9421-9a5eb903cf86",
+    title: "Ignition",
+    vendor: "Inductive Automation Inc.",
+    version: "8.1.11",
+    description: "The Unlimited Platform for SCADA.",
     status: "uninstalled"
   }
 ];

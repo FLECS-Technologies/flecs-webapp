d6887e5eac8c64291e0a4a550db95d725c71f2f5
samuel-greising <sam@flecs.tech>
2021-11-19 16:57:22 +0100

    Implemented a Frame to fit the content of pages

--- src/pages/InstalledApps.js
+++ src/pages/InstalledApps.js
@@ -1,15 +1,26 @@
 import { useContext, React } from 'react'
-import Box from '@mui/material/Box'
 import InstalledAppsList from '../components/InstalledAppsList'
-// import { installedAppsList } from "../data/DeviceAppsData.js";
 import { ReferenceDataContext } from '../data/ReferenceDataContext'
+import { styled } from '@mui/styles'
+
+const Header = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  minHeight: '48', // if minHeight is not set to 48 (or any other value), a minHeight of 64px is used, which makes the menu move down.
+  // padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar
+}))
 
 export default function installedApps () {
   const { appList } = useContext(ReferenceDataContext)
 
   return (
-        <Box s={{ mt: 5, mr: 10, ml: 32 }} aria-label="installed-apps-page">
-            <InstalledAppsList appData={appList} />
-        </Box>
+
+  <div>
+    <Header/>
+    <InstalledAppsList appData={appList} />
+  </div>
   )
 }

d1679c137e80fa0da30656d1dd1dbfe79b5df692
samuel-greising <sam@flecs.tech>
2021-11-23 17:17:36 +0100

    removed jest config and removed absolute paths

--- src/components/MarketplaceList.test.js
+++ src/components/MarketplaceList.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render /*, screen */ } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { marketPlaceAppsList } from '../data/MarketPlaceAppsList'
+// import { marketPlaceAppsList } from '../data/MarketPlaceAppsList'
 import MPList from './MarketplaceList'
 
 describe('Marketplace List', () => {
   test('renders marketplace list component', () => {
-    render(<MPList appData={marketPlaceAppsList} />)
+    render(<MPList /* appData={marketPlaceAppsList} */ />)
 
     // screen.debug()
   })

1397cd3a023aa11a544b511e888e80f5be03d621
samuel-greising <sam@flecs.tech>
2022-08-11 16:43:02 +0200

    fix app search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,41 +16,13 @@
  * limitations under the License.
  */
 
-import React, { useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Paper, IconButton, Divider, Autocomplete, TextField } from '@mui/material'
-import { FilterList, Clear, Search } from '@mui/icons-material'
+import { Paper, IconButton, Autocomplete, TextField } from '@mui/material'
+import { FilterList, Search } from '@mui/icons-material'
 
 const SearchBar = (props) => {
-  const { defaultSearchValue, setSearch, searchTitle, searchAutocomplete, setToggleFilter } = props
-  const valueRef = useRef('') // creating a refernce for TextField Component
-
-  function search (event, reason) {
-    // this happens if the user types anything into the search field
-    setSearch(valueRef.current.value)
-  }
-
-  function onClose (event, reason) {
-    // this happens if the user clicks on one of the autocomplete options
-    if (event.keyCode !== 13 && reason && reason === 'selectOption') {
-      setSearch(event.currentTarget.textContent)
-    } else {
-      // prevent re-render
-      event.preventDefault()
-    }
-  }
-
-  function onChange (event, reason) {
-    // this happens if the user selects one of the autocomplete options and confirms with ENTER
-    if (event.keyCode === 13 && reason) {
-      setSearch(reason)
-    }
-  }
-
-  function clearSearch () {
-    valueRef.current.value = null
-    setSearch('')
-  }
+  const { defaultSearchValue, searchTitle, setToggleFilter, search } = props
 
   return (
     <Paper data-testid='search-bar' component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}>
@@ -61,43 +33,35 @@ const SearchBar = (props) => {
         <Autocomplete
             sx={{ ml: 1, flex: 1 }}
             freeSolo
+            clearOnEscape
             aria-label="autocomplete"
-            disableClearable={true}
-            onInputChange={search}
-            onChange={onChange}
-            onClose={onClose}
-            options={searchAutocomplete || []}
+            onChange={search}
+            options={[]}
+            value={defaultSearchValue}
             renderInput={(params) => (
                 <TextField
                     {...params}
                     aria-label='search-field'
                     data-testid='search-field'
                     autoFocus={true}
-                    inputRef={valueRef} // connecting inputRef property of TextField to the valueRef
                     variant='standard'
                     placeholder={searchTitle}
                     onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault() }}
                     InputProps={{
                       ...params.InputProps,
-                      disableUnderline: true,
-                      value: { defaultSearchValue }
+                      disableUnderline: true
                     }}
                 />
             )}
         />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton sx={{ p: '10px' }} aria-label="clear-all" onClick={clearSearch}>
-        <Clear />
-      </IconButton>
     </Paper>
   )
 }
 
 SearchBar.propTypes = {
   defaultSearchValue: PropTypes.string,
-  setSearch: PropTypes.func,
   setToggleFilter: PropTypes.func,
   searchTitle: PropTypes.string,
-  searchAutocomplete: PropTypes.array
+  search: PropTypes.func
 }
 export default SearchBar

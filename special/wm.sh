2c013cdb764b4860c1d4f85ac05c43bf88ffe826
Alexander Reichert <alex@flecs.tech>
2023-03-10 14:25:01 +0100

    wm: update location of WebApp on weidmueller devices

--- special/wm.sh
+++ special/wm.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
-
-sed -i 's#^\(.*\)"homepage":.*$#\1"homepage": "http://localhost/uc-addon-flecs/ui/",#g' package.json
-sed -i 's#^\(.*\)<Router basename=.*$#\1<Router basename=\x27/uc-addon-flecs/ui\x27>#g' src/index.js
+sed -i 's#^\(.*\)"homepage":.*$#\1"homepage": "http://localhost/u-os-app-flecs/ui/",#g' package.json
+sed -i 's#^\(.*\)<Router basename=.*$#\1<Router basename=\x27/u-os-app-flecs/ui\x27>#g' src/index.js

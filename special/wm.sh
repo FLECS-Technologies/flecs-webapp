ef252fb604162423e1c7c9648d67cc0ee6208b19
Alexander Reichert <alex@flecs.tech>
2023-07-13 14:31:04 +0200

    wm: replace hostname of flecs-webapp in u-os-app

--- special/wm.sh
+++ special/wm.sh
@@ -2,3 +2,4 @@
 
 sed -i 's#^\(.*\)"homepage":.*$#\1"homepage": "http://localhost/u-os-app-flecs/ui/",#g' package.json
 sed -i 's#^\(.*\)<Router basename=.*$#\1<Router basename=\x27/u-os-app-flecs/ui\x27>#g' src/index.js
+sed -i 's#host.docker.internal#u-os-app-flecs#g' docker/conf.d/default.conf

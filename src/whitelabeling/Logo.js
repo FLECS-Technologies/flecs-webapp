2c267e5608fccb12b0eb05f174e3842a9249a561
samuel-greising <sam@flecs.tech>
2024-03-15 16:18:27 +0100

    white label

--- src/whitelabeling/Logo.js
+++ src/whitelabeling/Logo.js
@@ -19,15 +19,20 @@
 import React from 'react'
 import { IconButton, Typography } from '@mui/material'
 import { ReactComponent as LogoSVG } from './logo.svg'
+import { ReactComponent as BILogo } from './bi.svg'
 
 export default function Logo () {
   return (
         <React.Fragment>
-            <IconButton aria-label='logo' disabled={true}>
-              <LogoSVG width="24" height="24" />
+            <IconButton aria-label='BI-Logo' disabled={true}>
+              <BILogo width='48' height='48' />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              FLECS
+            <Typography variant='caption' component='div' sx={{ flexGrow: 1 }}>
+              powered by
+              <IconButton aria-label='FLECS-Logo' disabled={true}>
+                <LogoSVG width='16' height='16' />
+              </IconButton>
+              <Typography variant='caption'>FLECS</Typography>
             </Typography>
         </React.Fragment>
   )

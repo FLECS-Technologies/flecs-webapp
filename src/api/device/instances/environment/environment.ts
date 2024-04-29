cefb198a35a22b4a49a62b1e489452e7ac647d52
samuel-greising <sam@flecs.tech>
2024-04-29 11:19:54 +0200

    implement api call to environment endpoint

---
+++ src/api/device/instances/environment/environment.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 FLECS Technologies GmbH
+ *
+ * Created on Thu Apr 25 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import axios from 'axios'
+import { DeviceAPIConfiguration } from '../../../api-config'
+
+export async function fetchEnvironments(instanceId: string) {
+  return axios
+    .get(
+      DeviceAPIConfiguration.TARGET +
+        DeviceAPIConfiguration.DEVICE_BASE_ROUTE +
+        DeviceAPIConfiguration.GET_INSTANCE_ENVIRONMENT_URL(instanceId)
+    )
+    .then((response) => {
+      return response.data as string[]
+    })
+    .catch((error) => {
+      return Promise.reject(error)
+    })
+}
+
+export async function putEnvironments(instanceId: string, envs: string[]) {
+  return axios
+    .put(
+      DeviceAPIConfiguration.TARGET +
+        DeviceAPIConfiguration.DEVICE_BASE_ROUTE +
+        DeviceAPIConfiguration.GET_INSTANCE_ENVIRONMENT_URL(instanceId),
+      { environment: envs }
+    )
+    .then()
+    .catch((error) => {
+      return Promise.reject(error)
+    })
+}

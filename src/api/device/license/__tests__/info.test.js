25fd6da4d32c81d97c3560a3e9f723a1de7eb2bc
samuel-greising <sam@flecs.tech>
2024-06-10 11:25:33 +0200

    FLX-652 create and extend unit tests

---
+++ src/api/device/license/__tests__/info.test.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 FLECS Technologies GmbH
+ *
+ * Created on Mon Jun 10 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import '@testing-library/dom'
+import { waitFor } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import axios from 'axios'
+import { LicenseInfoAPI } from '../info'
+import { mockLicenseInfoAPIResponse } from '../__mocks__/info'
+
+jest.mock('axios')
+
+describe('LicenseInfoAPI', () => {
+  beforeAll(() => {
+    axios.get = jest.fn()
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+  test('calls successful LicenseInfoAPI', async () => {
+    axios.get.mockResolvedValueOnce(mockLicenseInfoAPIResponse)
+    const response = await waitFor(() => LicenseInfoAPI())
+
+    expect(response.isValid).toBe(mockLicenseInfoAPIResponse.data.isValid)
+  })
+
+  test('calls unsuccessful LicenseInfoAPI', async () => {
+    axios.get.mockRejectedValueOnce(new Error('Fetching license info failed.'))
+    await act(async () => {
+      expect(LicenseInfoAPI()).rejects.toThrow()
+    })
+  })
+})
---
+++ src/components/device/license/__tests__/LicenseInfo.test.tsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 FLECS Technologies GmbH
+ *
+ * Created on Mon Jun 10 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { render, act, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import LicenseInfo from '../LicenseInfo'
+import { mockLicenseInfoAPIResponse } from '../../../../api/device/license/__mocks__/info'
+
+jest.mock('../../../../api/device/license/info')
+
+describe('LicenseInfo Component', () => {
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  it('Show license info', async () => {
+    render(<LicenseInfo />)
+    await waitFor(() => {
+      const licenseText = screen.getByText(
+        mockLicenseInfoAPIResponse.data.license
+      )
+      expect(licenseText).toBeInTheDocument()
+
+      const typeText = screen.getByText(mockLicenseInfoAPIResponse.data.type)
+      expect(typeText).toBeInTheDocument()
+
+      const sessionIdText = screen.getByText(
+        mockLicenseInfoAPIResponse.data.sessionId.id
+      )
+      expect(sessionIdText).toBeInTheDocument()
+
+      const timestamp = screen.getByText(
+        String(mockLicenseInfoAPIResponse.data.sessionId.timestamp)
+      )
+      expect(timestamp).toBeInTheDocument()
+    })
+  })
+})

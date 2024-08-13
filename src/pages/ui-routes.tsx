a83fc2e5c2eb45f2c94cb64c710fc522a8d3f0df
samuel-greising <sam@flecs.tech>
2024-08-13 09:23:18 +0200

    FLX-649 Remove user login from marketplace and convert ui routes to typescript

---
+++ src/pages/ui-routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import InstalledApps from './InstalledApps'
+import Marketplace from './Marketplace'
+import ServiceMesh from './ServiceMesh'
+import System from './System'
+import NotFound from './NotFound'
+import Login from './Login'
+import OpenSource from './OpenSource'
+
+const UIRoutes: React.FC = () => {
+  return useRoutes([
+    { path: '/', element: <InstalledApps /> },
+    { path: '/marketplace', element: <Marketplace /> },
+    { path: 'service-mesh', element: <ServiceMesh /> },
+    { path: '/system', element: <System /> },
+    { path: '/login', element: <Login /> },
+    { path: '/open-source', element: <OpenSource /> },
+    { path: '*', element: <NotFound /> }
+  ])
+}
+
+export { UIRoutes }

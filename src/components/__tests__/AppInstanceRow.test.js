fd77bcdd834cc701cefd5a4d52f52aefd29a59bf
samuel-greising <sam@flecs.tech>
2024-11-04 16:54:43 +0100

    FLX-830 prompt before deleting an instance

--- src/components/__tests__/AppInstanceRow.test.js
+++ src/components/__tests__/AppInstanceRow.test.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2021 FLECS Technologies GmbH
  *
- * Created on Tue Nov 30 2021
+ * Created on Mon Nov 04 2024
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,121 +17,154 @@
  */
 
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import AppInstanceRow from '../AppInstanceRow'
-import { JobsContextProvider } from '../../data/JobsContext'
+import { JobsContext } from '../../data/JobsContext'
+import AppAPI from '../../api/device/AppAPI'
 
-describe('AppInstanceRow', () => {
-  function loadReferenceData() {}
-  const testApp = {
-    multiInstance: true,
-    editors: [{ name: 'editor', url: '/editor' }],
-    appKey: {
-      name: 'Test app',
-      version: 'Test App Version'
-    }
-  }
-  const testAppInstance = {
-    instanceId: '01234567',
-    instanceName: 'Smarthome',
-    status: 'running',
-    appKey: {
-      version: 'Test App Version'
-    },
+jest.mock('../../api/device/AppAPI')
+
+describe('AppInstanceRow Component', () => {
+  const mockApp = {
+    title: 'Test App',
+    appKey: { version: '1.0.0', name: 'Test App' },
+    instances: [
+      { instanceId: '1', instanceName: 'Test Instance', status: 'stopped' }
+    ],
     editors: [{ name: 'editor', url: '/editor' }]
   }
-  test('render running instance and stop instance', () => {
-    testAppInstance.status = 'running'
-    const { getByLabelText } = render(
-      <JobsContextProvider>
-        <AppInstanceRow
-          loadAppReferenceData={loadReferenceData}
-          app={testApp}
-          appInstance={testAppInstance}
-        />
-      </JobsContextProvider>
-    )
-    const stopButton = getByLabelText('stop-instance-button')
-    const startButton = getByLabelText('start-instance-button')
-    const deleteButton = getByLabelText('delete-instance-button')
-    const infoButton = getByLabelText('instance-info-button')
+  const mockAppInstance = {
+    instanceId: '1',
+    instanceName: 'Test Instance',
+    appKey: mockApp.appKey,
+    status: 'stopped',
+    editors: [{ name: 'editor', url: '/editor' }]
+  }
+  const mockSetFetchingJobs = jest.fn()
 
-    fireEvent.click(stopButton)
-
-    expect(stopButton).toBeDisabled()
-    expect(startButton).toBeVisible()
-    expect(deleteButton).toBeVisible()
-    expect(infoButton).toBeVisible()
+  beforeEach(() => {
+    jest.clearAllMocks()
   })
 
-  test('render running instance and delete instance', () => {
-    testAppInstance.status = 'running'
-    const { getByLabelText } = render(
-      <JobsContextProvider>
-        <AppInstanceRow
-          loadAppReferenceData={loadReferenceData}
-          app={testApp}
-          appInstance={testAppInstance}
-        />
-      </JobsContextProvider>
+  function loadAppReferenceData() {}
+
+  function renderComponent() {
+    return render(
+      <JobsContext.Provider value={{ setFetchingJobs: mockSetFetchingJobs }}>
+        <table>
+          <tbody>
+            <AppInstanceRow
+              app={mockApp}
+              appInstance={mockAppInstance}
+              loadAppReferenceData={loadAppReferenceData}
+            />
+          </tbody>
+        </table>
+      </JobsContext.Provider>
     )
-    const stopButton = getByLabelText('stop-instance-button')
-    const startButton = getByLabelText('start-instance-button')
-    const deleteButton = getByLabelText('delete-instance-button')
-    const infoButton = getByLabelText('instance-info-button')
+  }
 
-    fireEvent.click(deleteButton)
-
-    expect(stopButton).toBeVisible()
-    expect(startButton).toBeVisible()
-    expect(deleteButton).toBeVisible()
-    expect(infoButton).toBeVisible()
+  test('renders component with app instance details', () => {
+    renderComponent()
+    expect(screen.getByText('Test Instance')).toBeInTheDocument()
+    expect(screen.getByText('1.0.0')).toBeInTheDocument()
   })
 
-  test('render stopped instance and start instance', () => {
-    testAppInstance.status = 'stopped'
-    const { getByLabelText } = render(
-      <JobsContextProvider>
-        <AppInstanceRow
-          loadAppReferenceData={loadReferenceData}
-          app={testApp}
-          appInstance={testAppInstance}
-        />
-      </JobsContextProvider>
-    )
-    const stopButton = getByLabelText('stop-instance-button')
-    const startButton = getByLabelText('start-instance-button')
-    const deleteButton = getByLabelText('delete-instance-button')
-    const infoButton = getByLabelText('instance-info-button')
+  test('shows start button and calls startInstance function when clicked', async () => {
+    AppAPI.prototype.startInstance = jest.fn().mockResolvedValueOnce(true)
+
+    renderComponent()
+    const startButton = screen.getByLabelText('start-instance-button')
 
     fireEvent.click(startButton)
 
-    expect(stopButton).toBeVisible()
-    expect(startButton).toBeDisabled()
-    expect(deleteButton).toBeVisible()
-    expect(infoButton).toBeVisible()
+    await waitFor(() => {
+      expect(AppAPI.prototype.startInstance).toHaveBeenCalledWith('1')
+      expect(mockSetFetchingJobs).toHaveBeenCalledTimes(1)
+    })
   })
 
-  test('renders an instance with an editor', () => {
-    const closeSpy = jest.fn()
-    window.open = jest.fn().mockReturnValue({ close: closeSpy })
+  test('shows stop button and calls stopInstance function when clicked', async () => {
+    AppAPI.prototype.stopInstance = jest.fn().mockResolvedValueOnce(true)
+    mockAppInstance.status = 'running'
 
-    const { getByLabelText } = render(
-      <JobsContextProvider>
-        <AppInstanceRow
-          loadAppReferenceData={loadReferenceData}
-          app={testApp}
-          appInstance={testAppInstance}
-        />
-      </JobsContextProvider>
-    )
+    renderComponent()
+    const stopButton = screen.getByLabelText('stop-instance-button')
 
-    const editorButton = getByLabelText('open-app-icon-button')
-    fireEvent.click(editorButton)
+    fireEvent.click(stopButton)
 
-    expect(editorButton).toBeEnabled()
-    expect(window.open).toHaveBeenCalled()
-    expect(window.open).toHaveBeenCalledWith('http://localhost/api/editor')
+    await waitFor(() => {
+      expect(mockSetFetchingJobs).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  test('disables start button if instance is running', () => {
+    mockAppInstance.status = 'running'
+    renderComponent()
+
+    const startButton = screen.getByLabelText('start-instance-button')
+    expect(startButton).toBeDisabled()
+  })
+
+  test('shows delete confirmation dialog when delete button is clicked', async () => {
+    renderComponent()
+    const deleteButton = screen.getByLabelText('delete-instance-button')
+
+    fireEvent.click(deleteButton)
+
+    expect(
+      screen.getByText('Remove Test Instance instance?')
+    ).toBeInTheDocument()
+  })
+
+  test('calls deleteInstance function after confirmation', async () => {
+    AppAPI.prototype.deleteInstance = jest.fn().mockResolvedValueOnce(true)
+
+    renderComponent()
+    const deleteButton = screen.getByLabelText('delete-instance-button')
+    fireEvent.click(deleteButton)
+
+    const confirmButton = screen.getByText('Remove Test Instance instance?')
+    fireEvent.click(confirmButton)
+  })
+
+  test('opens info dialog when info button is clicked', async () => {
+    renderComponent()
+    const infoButton = screen.getByLabelText('instance-info-button')
+
+    fireEvent.click(infoButton)
+
+    expect(screen.getByText('Info to Test Instance')).toBeInTheDocument()
+  })
+
+  test('opens settings dialog when settings button is clicked', async () => {
+    renderComponent()
+    const settingsButton = screen.getByLabelText('instance-settings-button')
+
+    fireEvent.click(settingsButton)
+
+    expect(screen.getByText('Settings of Test Instance')).toBeInTheDocument()
+  })
+
+  test('displays snackbar with success message on successful instance start', async () => {
+    AppAPI.prototype.startInstance = jest.fn().mockResolvedValueOnce(true)
+
+    renderComponent()
+    const startButton = screen.getByLabelText('start-instance-button')
+
+    fireEvent.click(startButton)
+  })
+
+  test('displays snackbar with error message on failed instance stop', async () => {
+    AppAPI.prototype.stopInstance = jest.fn().mockResolvedValueOnce(false)
+    AppAPI.prototype.lastAPIError = 'Error stopping instance'
+
+    renderComponent()
+    const stopButton = screen.getByLabelText('stop-instance-button')
+
+    fireEvent.click(stopButton)
+
+    await screen.findByText(/Failed to stop Test Instance/i)
   })
 })

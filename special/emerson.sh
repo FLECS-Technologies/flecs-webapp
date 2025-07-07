87e9901756e9d78f7874e24e0cbdbcfbe6f1b9aa
Lukas Simon <lukas@flecs.tech>
2025-07-07 12:18:47 +0200

    Adjust special build script for vite

--- special/emerson.sh
+++ special/emerson.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i 's#^\(.*\)"homepage":.*$#\1"homepage": "http://localhost/flecs/ui/",#g' package.json
-sed -i 's#^\(.*\)<Router basename=.*$#\1<Router basename=\x27/flecs/ui\x27>#g' src/index.js
+sed -i 's#^\(.*\)base: .*\x27\(.*\)$#\1base: \x27/flecs/ui/\x27\2#g' vite.config.ts
+sed -i 's#^\(.*\)<Router basename=.*$#\1<Router basename=\x27/flecs/ui\x27>#g' src/main.tsx
 
 sed -i 's#$scheme://$http_host#$scheme://localhost:40000#g' docker/conf.d/default.conf

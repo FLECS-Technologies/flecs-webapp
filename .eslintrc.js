e8b7b70bd88b62e2ff5a5387a0e0f0a451c77b85
Samuel Greising <sam@flecs.tech>
2026-02-09 08:50:57 +0100

    remove jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'standard',
-    'prettier'
-  ],
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'standard', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
